dfc253ad8b7042cf35672d9180734583a871520b
a97022 <32208544+a97022@users.noreply.github.com>
2021-10-26 18:15:07 +0100

    Update TPC3 BubbleSort.py

--- TPC3 BubbleSort.py
+++ TPC3 BubbleSort.py
@@ -13,25 +13,21 @@ def criarListaR():
 
 def bubbleSort ():
     lista2 = criarListaR()
-    alterou = True
-    tentativas= 0
+    alterou = 1
+    tentativas= 1
     i=0
-    while alterou == True:
-        #alterou = False
+    while alterou != 0:
+        alterou = 0
         for i in range (len(lista2)-1):
             if lista2[i] > lista2[i+1]:
                 lista2[i], lista2[i+1] = lista2[i+1], lista2[i]
                 i=i+1
-                alterou = True
-                tentativas = tentativas +1
-                print (lista2)
+                tentativas = tentativas + 1
             else:
                 tentativas = tentativas + 1
                 i=i+1
-                print (lista2)
-        
-    #if tentativas > n**2
-        #return (lista)
+    print(lista2)
+    return (lista2)
         
         
 bubbleSort()
